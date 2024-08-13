09ff4ac5651c6e5f8017b60742068e7aa605e64d
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-08-13 09:34:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const perguntas = [
     
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
@@ -65,16 +66,16 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++
-            mostraPergunta();
-        })
-
-
-        caixaAlternativas.appendChild(botaoAlternativas);
-
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas)
     }
+}
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacao;
+    atual++;
+    mostraPergunta();
 }
 
 
